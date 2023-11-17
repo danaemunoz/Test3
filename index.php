4e4489c79ac3ef4affc23a490637ff377a68b2d8
Javiera <jmunozv@ing.ucsc.cl>
2023-11-17 10:45:26 -0300

    test3

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link type="text/css" rel="stylesheet" href="index.css">
+    <title>TEST3</title>
+</head>
+<body>
+   <div class="imagensodimac">
+    <img src="sodimac.png" alt=""> 
+   </div>
+       
+    
+</body>
+</html> 
